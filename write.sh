4211d2408a415333560619bf6e752a0ef28e9b47
sirwillyum <55957911+sirwillyum@users.noreply.github.com>
2020-12-08 00:17:17 -0500

    Ubuntu Version
    Exclusive to Ubuntu Server and Debian based OS's

--- write.sh
+++ write.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #Ascending order with line ranges to assign usernames with 4 split text files used for input below
 sed -n '1,2p' /home/wtrue/Lab2/usernames.txt > admins.txt
 sed -n '3,13p' /home/wtrue/Lab2/usernames.txt > devs.txt
@@ -8,29 +9,32 @@ sed -n '25,220p' /home/wtrue/Lab2/usernames.txt > temp.txt
 file=/home/wtrue/Lab2/admins.txt #Declare text file to a variable
 write=$(cat "$file") #Read text file and make that read equal to variable write
 for i in $write #Loop until it finishes processing every line iteration
-do useradd -G admin $i #Iterate the following users to the admin group
-   usermod -a -G wheel $i #Also assign every admin to the wheel/sudo permissions group
+do useradd $i #Add users by processing each line
+   adduser $i admin #Iterate the following users to the admin group
+   adduser $i sudo #Also assign every admin to the sudo permissions group   
 done	
 
 file=/home/wtrue/Lab2/devs.txt #Declare text file to a variable
 write=$(cat "$file") #Read text file and make that read equal to variable write
 for i in $write #Loop until it finishes processing every line iteration
-do useradd -G developers $i #Iterate the following users to the developers group
-   usermod -s /bin/csh $i #Default developers to c shell
+do useradd $i #Add users by processing each line
+   adduser $i developers #Iterate the following users to the developers group
+   usermod -s /bin/csh $i
 done
 
 file=/home/wtrue/Lab2/staff.txt #Declare text file to a variable
 write=$(cat "$file") #Read text file and make that read equal to variable write
 for i in $write #Loop until it finishes processing every line iteration
-do useradd -G staff $i #Iterate the following users to the staff group
- 
+do useradd $i #Add users by processing each line
+   adduser $i staff #Iterate the following users to the staff group
 done
 
 file=/home/wtrue/Lab2/temp.txt #Declare text file to a variable
 write=$(cat "$file") #Read text file and make that read equal to variable write
 for i in $write #Loop until it finishes processing every line iteration
-do useradd -G temp $i #Iterate the following users to the temp group
-
+do useradd $i #Add users by processing each line
+   adduser $i temp  #Iterate the following users to the temp group
+   
 done
 
 
